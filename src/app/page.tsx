6058de62e8e60edfe78feb587b16f7dcd746535c
Saumya Dubey <saumya@Saumyas-MacBook-Air.local>
2024-01-16 17:48:53 +0530

    inline chips and input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
 
   const renderOptionsList = () => {
     return (
-      <div className="flex flex-col max-h-[350px] overflow-auto scrollbar absolute top-[70px] left-0 bg-gray-100 min-w-[450px]">
+      <div className="flex flex-col max-h-[350px] overflow-auto scrollbar absolute top-0 left-0 bg-gray-100 min-w-[450px]">
         {filteredOptions.map((item) => {
           return (
             <div onClick={() => addToOptions(item)} key={item.email} className="hover:bg-gray-200 rounded-md cursor-pointer py-2 px-4 flex justify-between items-center gap-4">
@@ -59,12 +59,10 @@ export default function Home() {
   const displaySelectedValueChips = () => {
     const options = new Set(selectedOptions);
     return (
-      <div className="flex flex-wrap gap-2 text-gray-600 justify-start">
+      <>
         {Array.from(options).map((item) => {
           return (
             <div className="p-1 rounded-lg flex gap-2 items-center min-w-fit" style={{ border: `1.5px solid ${item.color}`, backgroundColor: item.chipColor }} key={item.email}>
-            {/* <div className="flex flex-wrap gap-2 text-gray-600 justify-start w-full">
-            <div className="flex gap-2 text-gray-600 justify-start flex-wrap w-full"> */}
               <div className="w-6 h-6 rounded-full flex justify-center items-center text-white text-sm" style={{ backgroundColor: item.color}}>{item.name.charAt(0)}</div>
               <div className="text-sm">{item.name}</div>
               <div onClick={() => removeFromOptions(item.email)} className="hover:shadow-lg cursor-pointer transition-all duration-300 rounded-full">
@@ -73,7 +71,7 @@ export default function Home() {
             </div>
           )
         })}
-      </div>
+      </>
     )
   }
 
@@ -84,26 +82,24 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24 p-12 transition-all duration-500">
-      <div className="flex flex-col gap-2 bg-gray-50 p-2">
+      <div className="bg-gray-50 p-2">
         <div
-          className="bg-white md:min-w-[400px] md:w-[1000px] rounded-lg flex hover:shadow-sm transition-all focus:shadow-md duration-500 flex-wrap items-center gap-2"
+          className="bg-white md:min-w-[400px] md:w-[1000px] rounded-lg flex hover:shadow-sm transition-all focus:shadow-md duration-500 flex-wrap items-center gap-2 p-2"
         >
           {
             selectedOptions.length > 0 && displaySelectedValueChips()
           }
-          <div className="relative w-full flex-1">
             <input
               ref={inputRef}
               onClick={handleShowDropdown}
               placeholder={selectedOptions.length > 0 ? "" : "Add new User..."}
-              className="min-w-[100px] w-full p-4 outline-none cursor-text focus:shadow-md rounded-lg"
+              className="min-w-[100px] flex-1 p-4 outline-none cursor-text"
               value={searchQuery}
               onChange={handleSearchChange}
             />
             {
               isDropdownVisible && renderOptionsList()
             }
-          </div>
         </div>
       </div>
     </main>
